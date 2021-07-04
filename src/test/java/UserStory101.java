c3e7dd31b925d6d6487fb12ff9229f2a89b3c0f1
Karimzhon Masaitov <kmasaitov1988@gmail.com>
2021-07-04 10:58:07 -0500

    Started user story 101 nas been completed

--- src/test/java/UserStory101.java
+++ src/test/java/UserStory101.java
@@ -2,4 +2,6 @@ public class UserStory101 {
     //work commit 1
     //work commit 2
     //work commit 3
+    //Work han been finished and ready to push to remote
+    //so I can get my code reviewed and merged into the master in the codebase
 }
