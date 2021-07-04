92074fa3df5aefda841e724bf6470f2ac135f5e7
kmasaitov <82942992+kmasaitov@users.noreply.github.com>
2021-07-04 12:26:08 -0500

    Us102 has been ompleted (#2)
    Combined all commits by squashing

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,9 @@
+public class UserStory102 {
+    //lots of code written here
+    //Ready to push this branch to remote
+    //and open pull request so I can start collaborating
+    //so my team members can comment, review and approve or request for more changes
+
+
+    //I added more changes as requested
+}
